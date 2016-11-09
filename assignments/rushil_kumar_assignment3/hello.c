499b9bc284d5f3a58ef2992c22c9872c4bde8c8b
Rushil <rukumar333@gmail.com>
2016-11-09 12:50:51 -0500

    Testing out device driver.

--- assignments/rushil_kumar_assignment3/hello.c
+++ assignments/rushil_kumar_assignment3/hello.c
@@ -3,14 +3,39 @@
 #include <linux/sched.h>
 #include <linux/miscdevice.h>
 #include <linux/fs.h>
+#include <linux/kernel.h>
+#include <asm/uaccess.h>
 MODULE_LICENSE("DUAL BSD/GPL");
 
 static ssize_t read_process_list(struct file * file, char * buf, size_t count, loff_t * ppos){
   struct task_struct *p;
-  /* for_each_process(p){ */
-    
-  /* } */
-  return count;
+  char str[count];
+  int len;
+  for_each_process(p){
+    char pid_string[25];
+    char ppid_string[25];
+    char cpu_string[10];
+    len = strlen(str);
+    if(len + 60 >= count)
+      return -EINVAL;
+    snprintf(pid_string, 25, "%d", p->pid);
+    strcat(str, "PID=");
+    strcat(str, pid_string);
+    strcat(str, " ");
+    snprintf(ppid_string, 25, "%d", p->parent->pid);
+    strcat(str, "PPID=");
+    strcat(str, ppid_string);
+    strcat(str, " ");
+    snprintf(cpu_string, 10, "%d", task_cpu(p));
+    strcat(str, "CPU=");
+    strcat(str, cpu_string);
+    strcat(str, " ");    
+  }
+  len = strlen(str);
+  if(copy_to_user(buf, str, len))
+    return -EINVAL;
+  *ppos = len;  
+  return len;
 }
 
 static const struct file_operations process_list_fops = {
@@ -24,12 +49,12 @@ static struct miscdevice process_list = {
   &process_list_fops
 };
 
-static int __init process_init(){
+static int __init process_init(void){
   struct task_struct *p;
-  for_each_process(p){
-    printk(KERN_ALERT "PID: %d", (int)task_pid_nr(p));
-  }
   int ret;
+  /* for_each_process(p){ */
+  /*   printk(KERN_DEBUG "PID: %d\n", (int)task_pid_nr(p)); */
+  /* } */
   ret = misc_register(&process_list);
   if(ret)
     printk(KERN_ERR "Unable to register process_list misc device\n");
@@ -38,7 +63,7 @@ static int __init process_init(){
 
 module_init(process_init);
 
-static void __exit process_exit(){
+static void __exit process_exit(void){
   misc_deregister(&process_list);
   printk(KERN_ALERT "Module exiting - process list\n");
 }
