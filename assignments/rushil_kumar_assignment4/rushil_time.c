e01e05674c0328a775248dad4a8cf2abe242196b
Rushil <rukumar333@gmail.com>
2016-12-05 20:06:39 -0500

    System call code added.

---
+++ assignments/rushil_kumar_assignment4/rushil_time.c
@@ -0,0 +1,21 @@
+#include <linux/linkage.h>
+#include <linux/export.h>
+#include <linux/time.h>
+#include <asm/uaccess.h>
+#include <linux/printk.h>
+#include <linux/slab.h>
+#include <linux/timekeeping.h>
+
+asmlinkage int sys_my_xtime(struct timespec *current_time){
+  if(!access_ok(VERIFY_WRITE,current_time,sizeof(struct timespec))){
+    printk(KERN_ERR "Invalid memory\n");
+    return -EFAULT;
+  }
+  current_time->tv_sec = 0;
+  current_time->tv_nsec = 0;
+  ktime_get_ts(current_time);
+  printk(KERN_DEBUG "Time: %ld\n", current->tv_nsec);
+  return 0;
+}
+
+EXPORT_SYMBOL(sys_my_xtime);
