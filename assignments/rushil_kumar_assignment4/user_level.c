10b0ba717aa8d2e1bddc695a2e31bf5af5cc421e
Rushil <rukumar333@gmail.com>
2016-12-06 23:10:33 -0500

    Testing bad code.

--- assignments/rushil_kumar_assignment4/user_level.c
+++ assignments/rushil_kumar_assignment4/user_level.c
@@ -5,12 +5,15 @@
 #include <errno.h>
 
 #define _sys_my_xtime_ 329
+#define _sys_bad_call_ 330
 
 char testSyscall();
+void testBadCall(char code);
 
 int main(){
-  if(testSyscall() == -1)
-    return -1;
+  /* if(testSyscall() == -1) */
+  /*   return -1; */
+  testBadCall(0);
   return 0;
 }
 
@@ -20,10 +23,30 @@ char testSyscall(){
   ret = syscall(_sys_my_xtime_, my_timespec);
   if(ret < 0){
     /* perror("my_xtime()"); */
-    printf("Syscall return value :%d\n",ret);
+    printf("ERROR: Syscall return value: %d\n",ret);
     return -1;
   }
   printf("Time in nanoseconds: %ld\n", my_timespec.tv_nsec);
   printf("Time in seconds: %ld\n", my_timespec.tv_sec);  
   return 0;
+}
+
+void testBadCall(char code){
+  switch(code){
+  case 0:
+    printf("Dividing by zero!\n");
+    break;
+  case 1:
+    printf("Deferencing null pointer\n");
+    break;
+  case 3:
+    printf("No return value\n");
+    break;
+  }
+  int ret = syscall(_sys_bad_call_, code);
+  if(ret < 0){
+    printf("ERROR: Syscall return value: %d\n",ret);
+  }else{    
+    printf("Success: Syscall return value: %d\n", ret);
+  }
 }
